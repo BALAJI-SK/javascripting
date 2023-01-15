1507fbea8d99d1636600809872444d15427bec57
S K BALAJI <skbalajimbl1@gmail.com>
2023-01-15 17:06:19 +0530

    feat: add function that checks same digits in Number

---
+++ testing/digit_utilty.test.js
@@ -0,0 +1,34 @@
+// const {  isSameDigits, isSameDigitsArrow} = require('../digit_utilty');
+const  {isSameDigits,isSameDigitsArrow  }= require('../digit_utilty');
+
+// Test Regular Function
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigits(22)).toEqual(true);
+})
+
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigits(222222)).toEqual(true);
+})
+
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigits(223)).toEqual(false);
+})
+
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigits(2234)).toEqual(false);
+})
+
+//Test Arrow function
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigitsArrow(223)).toEqual(false);
+})
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigitsArrow(2134)).toEqual(false);
+})
+test('Checking every digit in number is same or not',()=>{
+    expect(isSameDigitsArrow(22222222)).toEqual(true);
+})
+test('Checking every digit in number is same or not',()=>{
+  
+    expect(isSameDigitsArrow(22)).toEqual(true);
+})
