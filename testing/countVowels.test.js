0e1a24f8f908b767820b745acf1543414b359975
S K BALAJI <skbalajimbl1@gmail.com>
2023-01-15 19:09:29 +0530

    feat: function write & testing for counting vowels

---
+++ testing/countVowels.test.js
@@ -0,0 +1,28 @@
+const {numberOfVowels,numberOfVowelsArrow }=require('../countVowels')
+
+test('Count Vowels',()=>{
+expect(numberOfVowels("codeacademy")).toBe(5);
+})
+test('Count Vowels',()=>{
+    expect(numberOfVowels('azxsws')).toBe(1);
+})
+test('Count Vowels',()=>{
+        expect(numberOfVowels('qwee')).toBe(2);
+})
+test('Count Vowels',()=>{
+expect(numberOfVowels('qqqqqqqqqq')).toBe(0);
+})
+
+
+test('Count Vowels',()=>{
+    expect(numberOfVowelsArrow("codeacademy")).toBe(5);
+    })
+test('Count Vowels',()=>{
+expect(numberOfVowelsArrow('qqqqqqq')).toBe(0);
+ })
+test('Count Vowels',()=>{
+expect(numberOfVowelsArrow('qwiiii')).toBe(4);
+})
+test('Count Vowels',()=>{
+expect(numberOfVowelsArrow('utttree')).toBe(3);
+})
