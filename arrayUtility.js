26fd8ce5c8464473124377627029eb768e2691e4
S K BALAJI <skbalajimbl1@gmail.com>
2023-01-17 08:40:54 +0530

    feat: function triple array with only even integer using reduce function

--- arrayUtility.js
+++ arrayUtility.js
@@ -7,10 +7,25 @@ const doubleArray = (numbers) => {
 
 const tripleArray = (numbers) => {
   if (!Array.isArray(numbers)) { throw new Error('unspported input'); }
-  return numbers.map((number) => {
-    if (Number.isInteger(number)) { throw new Error('unspported datatype input'); }
-    return number * 3;
-  }).filter(number => (number % 2 == 0));
+  if (numbers.length == 0) return [];
+  //using reduce
+  return numbers.reduce((accumulator, currentValue) => {
+    if (Number.isInteger(currentValue)) {
+      console.log(currentValue);
+      if (currentValue % 2 == 0) {
+        let tripled = currentValue * 3;
+        return [...accumulator, tripled];
+      }
+    }
+    else {
+      throw new Error('unspported datatype input');
+    }
+    return accumulator;
+  }, []);
 };
 // console.log(tripleArray([1,2,3,'a']));
 module.exports = { doubleArray, tripleArray };
+// return numbers.map((number) => {
+//   if (Number.isInteger(number)) { throw new Error('unspported datatype input'); }
+//   return number * 3;
+// }).filter(number => (number % 2 == 0));
